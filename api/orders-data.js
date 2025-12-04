0edc0f4efb8de72bf345137e60d0a7208bdb8ff9
icefut <112353382+icefut@users.noreply.github.com>
2025-12-05 00:07:38 +0100

    Create orders-data.js

---
+++ api/orders-data.js
@@ -0,0 +1,13 @@
+// /api/orders-data.js
+module.exports = [
+  {
+    order_number: "22104",
+    email: "dennis@example.com",
+    tracking_number: "4PX300230247C",
+  },
+  {
+    order_number: "22105",
+    email: "eby@example.com",
+    tracking_number: "4PX300230247S",
+  },
+];
